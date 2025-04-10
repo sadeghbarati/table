1ce684f8b4f0512b675c198a9f17092bf4d3ee06
Sadegh Barati <sadeghbaratiwork@gmail.com>
2025-04-10 17:28:23 +0330

    chore: push

--- examples/react/column-pinning-sticky/src/main.tsx
+++ examples/react/column-pinning-sticky/src/main.tsx
@@ -19,9 +19,9 @@ import { faker } from '@faker-js/faker'
 const getCommonPinningStyles = (column: Column<Person>): CSSProperties => {
   const isPinned = column.getIsPinned()
   const isLastLeftPinnedColumn =
-    isPinned === 'left' && column.getIsLastColumn('left')
+    isPinned === 'start' && column.getIsLastColumn('start')
   const isFirstRightPinnedColumn =
-    isPinned === 'right' && column.getIsFirstColumn('right')
+    isPinned === 'end' && column.getIsFirstColumn('end')
 
   return {
     boxShadow: isLastLeftPinnedColumn
@@ -29,8 +29,8 @@ const getCommonPinningStyles = (column: Column<Person>): CSSProperties => {
       : isFirstRightPinnedColumn
         ? '4px 0 4px -4px gray inset'
         : undefined,
-    left: isPinned === 'left' ? `${column.getStart('left')}px` : undefined,
-    right: isPinned === 'right' ? `${column.getAfter('right')}px` : undefined,
+    insetInlineStart: isPinned === 'start' ? `${column.getStart('start')}px` : undefined,
+    insetInlineEnd: isPinned === 'end' ? `${column.getAfter('end')}px` : undefined,
     opacity: isPinned ? 0.95 : 1,
     position: isPinned ? 'sticky' : 'relative',
     width: column.getSize(),
@@ -99,6 +99,7 @@ function App() {
     debugHeaders: true,
     debugColumns: true,
     columnResizeMode: 'onChange',
+    columnResizeDirection: 'ltr'
   })
 
   const randomizeColumns = () => {
@@ -180,14 +181,14 @@ function App() {
                       </div>
                       {!header.isPlaceholder && header.column.getCanPin() && (
                         <div className="flex gap-1 justify-center">
-                          {header.column.getIsPinned() !== 'left' ? (
+                          {header.column.getIsPinned() !== 'start' ? (
                             <button
                               className="border rounded px-2"
                               onClick={() => {
-                                header.column.pin('left')
+                                header.column.pin('start')
                               }}
                             >
-                              {'<='}
+                              {'Start/Right'}
                             </button>
                           ) : null}
                           {header.column.getIsPinned() ? (
@@ -200,14 +201,14 @@ function App() {
                               X
                             </button>
                           ) : null}
-                          {header.column.getIsPinned() !== 'right' ? (
+                          {header.column.getIsPinned() !== 'end' ? (
                             <button
                               className="border rounded px-2"
                               onClick={() => {
-                                header.column.pin('right')
+                                header.column.pin('end')
                               }}
                             >
-                              {'=>'}
+                              {'End/Left'}
                             </button>
                           ) : null}
                         </div>
